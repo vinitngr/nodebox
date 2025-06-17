12d8b20ccfbabc59c222078809c93be084fdab2a
Vinit <vinitnagar56@gmail.com>
2025-06-17 12:15:12 +0000

    separate out types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,3 +7,13 @@ export type LogStore = {
   logs: Log[];
   addLog: (type: Log['type'], msg: string) => void;
 };
+
+export interface ContainerFile {
+    [name: string]: {
+        file: {
+            contents: string | Uint8Array;
+        };
+    };
+}
+
+export type Option = "github" | "folder"
